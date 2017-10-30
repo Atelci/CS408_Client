8366474171acbdfe3c4559d88a82d63ab7c1347c
AYasin <ayasinakalin@gmail.com>
2017-10-30 19:41:59 +0300

    Add form boxes

--- CS408Client/CS408Client/Form1.Designer.cs
+++ CS408Client/CS408Client/Form1.Designer.cs
@@ -29,18 +29,29 @@
         private void InitializeComponent()
         {
             this.groupBox_Main = new System.Windows.Forms.GroupBox();
-            this.groupBox_Side = new System.Windows.Forms.GroupBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.disconButton = new System.Windows.Forms.Button();
+            this.portText = new System.Windows.Forms.Label();
+            this.ipText = new System.Windows.Forms.Label();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.ipTextBox = new System.Windows.Forms.TextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.groupBox_Side = new System.Windows.Forms.GroupBox();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.usernameText = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.logText = new System.Windows.Forms.Label();
             this.groupBox_Main.SuspendLayout();
-            this.groupBox_Side.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.groupBox_Side.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox_Main
@@ -55,9 +66,89 @@
             this.groupBox_Main.Text = "MAIN MENU";
             this.groupBox_Main.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 49);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(827, 739);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.disconButton);
+            this.panel1.Controls.Add(this.portText);
+            this.panel1.Controls.Add(this.ipText);
+            this.panel1.Controls.Add(this.portTextBox);
+            this.panel1.Controls.Add(this.ipTextBox);
+            this.panel1.Controls.Add(this.connectButton);
+            this.panel1.Location = new System.Drawing.Point(3, 145);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(824, 282);
+            this.panel1.TabIndex = 0;
+            // 
+            // disconButton
+            // 
+            this.disconButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.disconButton.Location = new System.Drawing.Point(622, 219);
+            this.disconButton.Name = "disconButton";
+            this.disconButton.Size = new System.Drawing.Size(182, 46);
+            this.disconButton.TabIndex = 5;
+            this.disconButton.Text = "Disconnect";
+            this.disconButton.UseVisualStyleBackColor = true;
+            this.disconButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // portText
+            // 
+            this.portText.AutoSize = true;
+            this.portText.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.portText.Location = new System.Drawing.Point(554, 30);
+            this.portText.Name = "portText";
+            this.portText.Size = new System.Drawing.Size(153, 38);
+            this.portText.TabIndex = 4;
+            this.portText.Text = "Server Port";
+            // 
+            // ipText
+            // 
+            this.ipText.AutoSize = true;
+            this.ipText.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ipText.Location = new System.Drawing.Point(14, 30);
+            this.ipText.Name = "ipText";
+            this.ipText.Size = new System.Drawing.Size(147, 38);
+            this.ipText.TabIndex = 3;
+            this.ipText.Text = "IP Address";
+            this.ipText.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Location = new System.Drawing.Point(561, 78);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(243, 50);
+            this.portTextBox.TabIndex = 2;
+            // 
+            // ipTextBox
+            // 
+            this.ipTextBox.Location = new System.Drawing.Point(21, 78);
+            this.ipTextBox.Name = "ipTextBox";
+            this.ipTextBox.Size = new System.Drawing.Size(534, 50);
+            this.ipTextBox.TabIndex = 1;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.connectButton.Location = new System.Drawing.Point(622, 158);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(182, 46);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
             // groupBox_Side
             // 
-            this.groupBox_Side.Controls.Add(this.button1);
+            this.groupBox_Side.Controls.Add(this.refreshButton);
             this.groupBox_Side.Controls.Add(this.listBox1);
             this.groupBox_Side.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox_Side.Location = new System.Drawing.Point(857, 12);
@@ -68,13 +159,16 @@
             this.groupBox_Side.Text = "PLAYER LIST";
             this.groupBox_Side.Enter += new System.EventHandler(this.groupBox2_Enter);
             // 
-            // flowLayoutPanel1
+            // refreshButton
             // 
-            this.flowLayoutPanel1.Controls.Add(this.panel1);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(6, 49);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(827, 568);
-            this.flowLayoutPanel1.TabIndex = 0;
+            this.refreshButton.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.refreshButton.Location = new System.Drawing.Point(210, 557);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(185, 46);
+            this.refreshButton.TabIndex = 1;
+            this.refreshButton.Text = "Retrieve list";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
             // listBox1
             // 
@@ -83,67 +177,80 @@
             this.listBox1.ItemHeight = 32;
             this.listBox1.Location = new System.Drawing.Point(6, 49);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(389, 516);
+            this.listBox1.Size = new System.Drawing.Size(389, 484);
             this.listBox1.TabIndex = 0;
             // 
-            // button1
+            // panel2
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(210, 571);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 46);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.panel2.Controls.Add(this.usernameTextBox);
+            this.panel2.Controls.Add(this.usernameText);
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(821, 136);
+            this.panel2.TabIndex = 1;
             // 
-            // panel1
+            // usernameText
             // 
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(824, 232);
-            this.panel1.TabIndex = 0;
+            this.usernameText.AutoSize = true;
+            this.usernameText.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.usernameText.Location = new System.Drawing.Point(14, 23);
+            this.usernameText.Name = "usernameText";
+            this.usernameText.Size = new System.Drawing.Size(155, 38);
+            this.usernameText.TabIndex = 4;
+            this.usernameText.Text = "User Name";
             // 
-            // button2
+            // usernameTextBox
             // 
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button2.Location = new System.Drawing.Point(617, 158);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(185, 46);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.usernameTextBox.Location = new System.Drawing.Point(21, 64);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(534, 50);
+            this.usernameTextBox.TabIndex = 6;
             // 
-            // textBox1
+            // panel3
             // 
-            this.textBox1.Location = new System.Drawing.Point(21, 78);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(397, 50);
-            this.textBox1.TabIndex = 1;
+            this.panel3.Controls.Add(this.logText);
+            this.panel3.Controls.Add(this.logTextBox);
+            this.panel3.Location = new System.Drawing.Point(3, 433);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(821, 273);
+            this.panel3.TabIndex = 2;
             // 
-            // textBox2
+            // logTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(460, 78);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(344, 50);
-            this.textBox2.TabIndex = 2;
+            this.logTextBox.Location = new System.Drawing.Point(21, 54);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(783, 189);
+            this.logTextBox.TabIndex = 0;
+            this.logTextBox.Text = "";
+            // 
+            // logText
+            // 
+            this.logText.AutoSize = true;
+            this.logText.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.logText.Location = new System.Drawing.Point(14, 13);
+            this.logText.Name = "logText";
+            this.logText.Size = new System.Drawing.Size(62, 38);
+            this.logText.TabIndex = 6;
+            this.logText.Text = "Log";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1270, 694);
+            this.ClientSize = new System.Drawing.Size(1269, 769);
             this.Controls.Add(this.groupBox_Main);
             this.Controls.Add(this.groupBox_Side);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox_Main.ResumeLayout(false);
-            this.groupBox_Side.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.groupBox_Side.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -154,11 +261,20 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox_Side;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.TextBox ipTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button refreshButton;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label portText;
+        private System.Windows.Forms.Label ipText;
+        private System.Windows.Forms.Button disconButton;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label usernameText;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label logText;
+        private System.Windows.Forms.RichTextBox logTextBox;
     }
 }
 
